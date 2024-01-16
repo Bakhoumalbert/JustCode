c802d826d9c6f1c7ec91a6002f278ae14993d675
Albert Sandokh Bakhoum <bakhoum.albert@uam.edu.sn>
2024-01-16 16:49:29 +0000

    deploy project

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -1,16 +1,17 @@
+import { Backbutton } from '@/components/utils/BackButton'
 import { Breadcrumbs } from '@/components/utils/Breadcrump'
 import { PropsWithChildren } from 'react'
 
 export default function AdminLayout({ children }: PropsWithChildren) {
     return (
-        <>
+        <div>
             <div className="m-auto mt-4 max-w-2xl pl-4 ">
                 <div className='flex flex-row gap-4'>
-                    {/* <Backbutton /> */}
+                    <Backbutton />
                     <Breadcrumbs />
                 </div>
             </div>
             {children}
-        </>
+        </div>
     )
 }
